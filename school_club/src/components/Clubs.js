afc07e85e6017ba3a29f5c5db8a1093af024e0d5
Megha Shrestha <meghashrestha30@gmail.com>
2020-08-13 23:20:12 +0545

    component club and updated footer

--- school_club/src/components/Clubs.js
+++ school_club/src/components/Clubs.js
@@ -1,15 +1,74 @@
 import React from 'react';
-
-
-
-
-
+import science  from '../images/science.png';
+import art from '../images/art.png';
+import drama from '../images/drama.png';
+import music from '../images/music.png';
+import sports from '../images/sports.png';
+import photography from '../images/photography.png';
+import '../css/Club.css';
 
 class Clubs extends React.Component {
    
     render(){
         return (
-            <div >This is Clubs</div>
+                <React.Fragment>               
+                    <div class="site-section bg-info">
+                        <div class="container">
+                            <div class="row mb-5">
+                            <div class="col-12 text-center">
+                                <span class="text-cursive h5 text-red d-block">Pixies</span>
+                                <h2 class="text-white">OUR CLUBS</h2>
+                            </div>
+                            </div>
+                            <div class="row">
+                            <div class="col-lg-4 mb-4 mb-lg-0">
+                                <div class="package text-center bg-white">
+                                <span class="img-wrap"><img src={science} alt="Image" class="img-fluid" /></span>
+                                <h3 class="text-teal">SCIENCE CLUB</h3>
+                                <p><a href="#" class="btn btn-info btn-custom-1 mt-4">GET TO KNOW SOME MORE</a></p>
+                            </div>
+                            </div>
+                            <div class="col-lg-4 mb-4 mb-lg-0">
+                                <div class="package text-center bg-white">
+                                <span class="img-wrap"><img src={photography} alt="Image" class="img-fluid" /></span>
+                                <h3 class="text-warning">PHOTOGRAPHY</h3>
+                                <p><a href="#" class="btn btn-warning btn-custom-1 mt-4">GET TO KNOW SOME MORE</a></p>
+                                </div>
+                            </div>
+                            <div class="col-lg-4 mb-4 mb-lg-0">
+                                <div class="package text-center bg-white">
+                                <span class="img-wrap"><img src={music} alt="Image" class="img-fluid" /></span>
+                                <h3 class="text-secondary">MUSIC CLUB</h3>
+                                <p><a href="#" class="btn btn-secondary btn-custom-1 mt-4">GET TO KNOW SOME MORE</a></p>
+                                </div>
+                            </div>
+                            </div>
+                            <div class="row">
+                            <div class="col-lg-4 mb-4 mb-lg-0">
+                                <div class="package text-center bg-white">
+                                <span class="img-wrap"><img src={sports} alt="Image" class="img-fluid" /></span>
+                                <h3 class="text-success">SPORTS CLUB</h3>
+                                <p><a href="#" class="btn btn-success btn-custom-1 mt-4">GET TO KNOW SOME MORE</a></p>
+                            </div>
+                            </div>
+                            <div class="col-lg-4 mb-4 mb-lg-0">
+                                <div class="package text-center bg-white">
+                                <span class="img-wrap"><img src={art} alt="Image" class="img-fluid" /></span>
+                                <h3 class="text-teal">ART CLUB</h3>
+                                <p><a href="#" class="btn btn-info btn-custom-1 mt-4">GET TO KNOW SOME MORE</a></p>
+                                </div>
+                            </div>
+                            <div class="col-lg-4 mb-4 mb-lg-0">
+                                <div class="package text-center bg-white">
+                                <span class="img-wrap"><img src={drama} alt="Image" class="img-fluid" /></span>
+                                <h3 class="text-danger">DRAMA CLUB</h3>
+                                <p><a href="#" class="btn btn-primary btn-custom-1 mt-4">GET TO KNOW SOME MORE</a></p>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                </React.Fragment>  
         );
 }
 }
--- school_club/src/components/Footer.js
+++ school_club/src/components/Footer.js
@@ -1,103 +1,70 @@
 import React, { Component } from "react";
-// import "../css/footer.css";
-// import facebook from "../images/icons/001-facebook.png";
-// import twitter from "../images/icons/013-twitter.png";
-// import linkedin from "../images/icons/010-linkedin.png";
-// import insta from "../images/icons/011-instagram.png";
-// import messenger from "../images/icons/012-messenger.png";
+import "../css/footer.css";
+import facebook from "../images/icons/001-facebook.png";
+import twitter from "../images/icons/013-twitter.png";
+import linkedin from "../images/icons/010-linkedin.png";
+import insta from "../images/icons/011-instagram.png";
+import messenger from "../images/icons/012-messenger.png";
+import { Link } from 'react-router-dom'
+
 
 class Footer extends Component {
   render() {
     return (
-      // <div className="footer-container">
-      //             <div className="row">
-      //                 <div className="column">
-      //                     <ul className="list">
-      //                         <li><img src={facebook} alt= ""></img></li>
-      //                         <li><img src={twitter} alt= ""></img></li>
-      //                         <li><img src={linkedin} alt= ""></img></li>
-      //                         <li><img src={messenger} alt= ""></img></li>
-      //                         <li><img src={insta} alt= ""></img></li>
 
-      //                     </ul>
-      //                 </div>
-      //                 <div className="column">
-      //                     <ol className="list" >
-      //                         <text>
-      //                             <li>Address: New Baneshwor, Kathmandu</li>
-      //                             <li>Email:meghashrestha30@gmail.com </li>
-      //                             <li>Contact: +977-9812345678</li>
-      //                         </text>
-      //                     </ol>
-      //                 </div>
-      //             </div>
-      // </div>
 
       <footer className="site-footer">
         <div className="container">
           <div className="row">
             <div className="col-lg-4">
-              <h2 className="footer-heading mb-3">About Us</h2>
+              <h2 className="footer-heading mb-3">ABOUT US</h2>
               <p className="mb-5">
-                A platform to explore your creativity and have fun.{" "}
-              </p>
+                <ul className="list">
+                    <li><img src={facebook} alt= "" className="social-site-icon"></img></li>
+                    <li><img src={twitter} alt= "" className="social-site-icon"></img></li>
+                    <li><img src={linkedin} alt= "" className="social-site-icon"></img></li>
+                    <li><img src={messenger} alt= "" className="social-site-icon"></img></li>
+                    <li><img src={insta} alt= "" className="social-site-icon"></img></li>
+
+                  </ul>      
+                </p>
 
               
             </div>
             <div className="col-lg-8 ml-auto">
               <div className="row">
                 <div className="col-lg-4 ml-auto">
-                  <h2 className="footer-heading mb-4">Navigation</h2>
+                  <h2 className="footer-heading mb-4">QUICK LINKS</h2>
                   <ul className="list-unstyled">
-                    <li>
-                      <a href="/">About Us</a>
-                    </li>
-                    <li>
-                      <a href="/">Testimonials</a>
-                    </li>
-                    <li>
-                      <a href="/">Terms of Service</a>
-                    </li>
-                    <li>
-                      <a href="/">Privacy</a>
-                    </li>
-                    <li>
-                      <a href="/">Contact Us</a>
-                    </li>
+                  <li><Link to="/home">Home</Link></li>
+                  <li><Link to="/about">About</Link></li>
+                  <li><Link to="/clubs">Clubs</Link></li>
+                  <li><Link to="/gallery">Gallery</Link></li>
+                  <li><Link to="/my-clubs">My Clubs</Link></li>
                   </ul>
                 </div>
                 <div className="col-lg-4">
-                  <h2 className="footer-heading mb-4">Navigation</h2>
+                  <h2 className="footer-heading mb-4">CONTACT US</h2>
                   <ul className="list-unstyled">
+                   
                     <li>
-                      <a href="/">About Us</a>
+                      <a href="/">Address: kathmandu, Nepal</a>
                     </li>
                     <li>
-                      <a href="/">Testimonials</a>
+                      <a href="/">Email : clubadmin@gmail.com</a>
                     </li>
                     <li>
-                      <a href="/">Terms of Service</a>
-                    </li>
-                    <li>
-                      <a href="/">Privacy</a>
-                    </li>
-                    <li>
-                      <a href="/">Contact Us</a>
+                      <a href="/">Contact: +977-9812345678</a>
                     </li>
                   </ul>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="row pt-5 mt-5 text-center">
-            <div className="col-md-12">
-              <div className="border-top pt-5">
-                <p>
+                  <p className="copyright-footer">
                    Copyright &copy; All rights reserved.
                 </p>
-              </div>
-            </div>
-          </div></div>
+          </div>
+      </div>
        
       </footer>
     );
